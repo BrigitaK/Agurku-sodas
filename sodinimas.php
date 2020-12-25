e673370017236ad12bd237d478f88555b8652976
BrigitaK <70912981+BrigitaK@users.noreply.github.com>
2020-12-25 09:27:59 +0200

    php files

---
+++ sodinimas.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Agurkų sodas</title>
+</head>
+<style>
+    body {
+        margin: 50px 100px;
+    }
+    nav {
+        display: inline-block;
+        float: right;
+    }
+    nav a {
+        text-decoration: none;
+        color: black;
+        margin-left: 10px;
+    }
+</style>
+<body>
+    <nav>
+    <a href="">Sodinimas</a>
+    <a href="">Auginimas</a>
+    <a href="">Skynimas</a>
+    </nav>
+    
+</body>
+</html>

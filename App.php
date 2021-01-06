c0804fd197a04796c9851cdc35e716ec24d4c1bc
BrigitaK <70912981+BrigitaK@users.noreply.github.com>
2021-01-06 22:14:53 +0200

    App sodinimas static functions

--- App.php
+++ App.php
@@ -2,7 +2,88 @@
 
 class App {
 
-    function redirect($name) {
+    public static function session() {
+        if(!isset($_SESSION['a'])) {//jeigu nesetinta sesija. Gali buti nesetintas. Jei pirma karta ateini i puslapi, sitas masyvas bus tuscias.
+            $_SESSION['a'] = [];
+            $_SESSION['obj'] = []; // sukuriam objektu masyva, laikysim agurku objektus
+            $_SESSION['agurku ID'] = 0; //kad agurkai nesikartotu yra naujas kintamasis
+            $_SESSION['photo'] = '';
+        
+            $_SESSION['objP'] = []; // sukuriam objektu masyva, laikysim pomidoru objektus
+            $_SESSION['pomidoru ID'] = 0; //kad pomidorai nesikartotu yra naujas kintamasis
+            $_SESSION['photo'] = '';
+        
+            $_SESSION['objM'] = []; // sukuriam objektu masyva, laikysim pomidoru objektus
+            $_SESSION['moliugu ID'] = 0; //kad pomidorai nesikartotu yra naujas kintamasis
+            $_SESSION['photo'] = '';
+        }
+    }
+
+    public static function sodintiAgurka() {
+        $agurkoObj = new Agurkas($_SESSION['agurku ID']);//irasomas objektas, pasidarom nauja agurka
+        $_SESSION['agurku ID']++;
+        // norint ideti objekta i sesija reikia paversti i stringa ir atgal atversti i objekta
+        $_SESSION['obj'][]= serialize($agurkoObj); //irasom serializuota objekta paversta i stringa
+        App::redirect(sodinimas);
+    }
+
+    public static function sodintiPomidora() {
+        $pomidoroObj = new Pomidoras($_SESSION['pomidoru ID']);
+        ++$_SESSION['pomidoru ID'];
+        $_SESSION['objP'][]= serialize($pomidoroObj);
+        App::redirect(sodinimas);
+    }
+
+    public static function sodintiMoliuga() {
+        $moliugoObj = new Moliugas($_SESSION['moliugu ID']);
+        ++$_SESSION['moliugu ID'];
+        $_SESSION['objM'][]= serialize($moliugoObj);
+        App::redirect(sodinimas);
+    }
+
+    public static function sodintiVisasDarzoves() {
+        $moliugoObj = new Moliugas($_SESSION['moliugu ID']);
+        ++$_SESSION['moliugu ID'];
+        $_SESSION['objM'][]= serialize($moliugoObj);
+        $pomidoroObj = new Pomidoras($_SESSION['pomidoru ID']);
+        ++$_SESSION['pomidoru ID'];
+        $_SESSION['objP'][]= serialize($pomidoroObj);
+        $agurkoObj = new Agurkas($_SESSION['agurku ID']);
+        ++$_SESSION['agurku ID'];
+        $_SESSION['obj'][]= serialize($agurkoObj);
+        App::redirect(sodinimas);
+    }
+
+    public static function rautiAgurka(){
+        foreach($_SESSION['obj'] as $index => $agurkas) {
+            $agurkas = unserialize($agurkas);
+            if ($_POST['rauti'] == $agurkas->id) {
+                unset($_SESSION['obj'][$index]);
+                App::redirect(sodinimas);
+            }
+        }
+    }
+
+    public static function rautiPomidora(){
+        foreach($_SESSION['objP'] as $index => $pomidoras) {
+            $pomidoras = unserialize($pomidoras);
+            if ($_POST['rautiP'] == $pomidoras->id) {
+                unset($_SESSION['objP'][$index]);
+                App::redirect(sodinimas);
+            }
+        }
+    }
+
+    public static function rautiMoliuga(){
+        foreach($_SESSION['objM'] as $index => $moliugas) {
+            $moliugas = unserialize($moliugas);
+            if ($_POST['rautiM'] == $moliugas->id) {
+                unset($_SESSION['objM'][$index]);
+                App::redirect(sodinimas);
+            }
+        }
+    }
+    public static function redirect($name) {
         header("Location: http://localhost:8888/dashboard/agurkai/agurku-sodas/$name.php");
         die;
     }
--- sodinimas.php
+++ sodinimas.php
@@ -1,8 +1,5 @@
 <?php
 
-//padaryti pomidoru ir agurku teva
-//
-
 session_start();
 
 spl_autoload_register(function ($class){
@@ -23,137 +20,43 @@ spl_autoload_register(function ($class){
     }
 });
 
-
-//sukurti klase su statiniais metodais i augunimo metoda ir sesija su agurku id ir sodinimo koda
-//neliesti validaciju
-//app redirech auginimas
-//tevas turi tureti metoda k
-//klase app, turi tureti statiniu metodu ir tie statiniai metodai ir jie turi buti irasomi
-
 if(!isset($_SESSION['logged']) || 1 != $_SESSION['logged']) {
-    
     App::redirect(login);
 }
-if(!isset($_SESSION['a'])) {//jeigu nesetinta sesija. Gali buti nesetintas. Jei pirma karta ateini i puslapi, sitas masyvas bus tuscias.
-    $_SESSION['a'] = [];
-    $_SESSION['obj'] = []; // sukuriam objektu masyva, laikysim agurku objektus
-    $_SESSION['agurku ID'] = 0; //kad agurkai nesikartotu yra naujas kintamasis
-    $_SESSION['photo'] = '';
 
-    $_SESSION['objP'] = []; // sukuriam objektu masyva, laikysim pomidoru objektus
-    $_SESSION['pomidoru ID'] = 0; //kad pomidorai nesikartotu yra naujas kintamasis
-    $_SESSION['photo'] = '';
-
-    $_SESSION['objM'] = []; // sukuriam objektu masyva, laikysim pomidoru objektus
-    $_SESSION['moliugu ID'] = 0; //kad pomidorai nesikartotu yra naujas kintamasis
-    $_SESSION['photo'] = '';
-}
-
-//sodinimo scenarijus
-//reikia detektint sodinimo mygtuka, posto masyve turi atsirasti indeksas 'sodinti'
-//automatiskai generuojam indeksa $_SESSION['a'][] ir priskiriam jam nauja agurka
-//tuomet agurku id dides vienetu
+App::session();
 
 
 if(isset($_POST['sodinti'])) {
-
-    //sukuriam konstruktoriu
-    $agurkoObj = new Agurkas($_SESSION['agurku ID']);//irasomas objektas, pasidarom nauja agurka
-    ++$_SESSION['agurku ID'];
-
-    // norint ideti objekta i sesija reikia paversti i stringa ir atgal atversti i objekta
-    $_SESSION['obj'][]= serialize($agurkoObj); //irasom serializuota objekta paversta i stringa
-
-    //kodel mes serializuojam? kadangi mes sita objekta saugosim, bet kokie saugjimai, jeigu mes neturime savo saugojimo irankiuose dokumentinese
-    //duomenu bazese, turime paversti i stringa. Sesija yra masyvas, ir i ji galim deti objektus ir isimti objektus. Verciam, nes jis ir taip bus paverstas
-    //php veikia trumpai, kai php uzbaigia savo darba visi objektai numirsta, todel laikyti to objekto ir kazko updatinti kai kazkas 
-    //ivyksta nera prasmes. Kadangi zinok kad objektas greit numirs, tai mes iskar ji serializuojam ir kai reikia vel i ji kreipiames
-    
-
-    //$_SESSION['a'][]= [ 
-    //    'id' => ++$_SESSION['agurku ID'],
-    //    'agurkai' => 0,
-    //];
-    App::redirect(sodinimas);
-    //po post einam antra karta, kad eitume per get
+    App::sodintiAgurka();
 }
 
 
 if(isset($_POST['sodintiP'])) {
-
-    $pomidoroObj = new Pomidoras($_SESSION['pomidoru ID']);//irasomas objektas, pasidarom nauja agurka
-    ++$_SESSION['pomidoru ID'];
-    $_SESSION['objP'][]= serialize($pomidoroObj); //irasom serializuota objekta paversta i stringa
-    
-    App::redirect(sodinimas);
+    App::sodintiPomidora();
 }
 
 if(isset($_POST['sodintiM'])) {
-
-    $moliugoObj = new Moliugas($_SESSION['moliugu ID']);//irasomas objektas, pasidarom nauja agurka
-    ++$_SESSION['moliugu ID'];
-    $_SESSION['objM'][]= serialize($moliugoObj); //irasom serializuota objekta paversta i stringa
-    
-    App::redirect(sodinimas);
+    App::sodintiMoliuga();
 }
 if(isset($_POST['sodintiV'])) {
-
-    $moliugoObj = new Moliugas($_SESSION['moliugu ID']);//irasomas objektas, pasidarom nauja agurka
-    ++$_SESSION['moliugu ID'];
-    $_SESSION['objM'][]= serialize($moliugoObj); //irasom serializuota objekta paversta i stringa
-    $pomidoroObj = new Pomidoras($_SESSION['pomidoru ID']);//irasomas objektas, pasidarom nauja agurka
-    ++$_SESSION['pomidoru ID'];
-    $_SESSION['objP'][]= serialize($pomidoroObj); //irasom serializuota objekta paversta i stringa
-    $agurkoObj = new Agurkas($_SESSION['agurku ID']);//irasomas objektas, pasidarom nauja agurka
-    ++$_SESSION['agurku ID'];
-    $_SESSION['obj'][]= serialize($agurkoObj); //irasom serializuota objekta paversta i stringa
-    
-    App::redirect(sodinimas);
+    App::sodintiVisasDarzoves();
 }
-
-//Jeigu norim atvaizduoti, tai darom su get
-//jei norim kazka nusiusti, tai einam su post
 
 //isrovimo scenarijus
 
 if(isset($_POST['rauti'])) {
-    //foreach($_SESSION['a'] as $index => $agurkas) {
-    //    if ($_POST['rauti'] == $agurkas[id]) {
-    //        unset($_SESSION['a'][$index];
-    //        header('Location: http://localhost:8888/dashboard/agurkai/agurku-sodas/sodinimas.php');
-    //        die;
-    //    }
-    //}
-
-    foreach($_SESSION['obj'] as $index => $agurkas) {
-        $agurkas = unserialize($agurkas);
-        if ($_POST['rauti'] == $agurkas->id) {
-            unset($_SESSION['obj'][$index]);
-            App::redirect(sodinimas);
-        }
-    }
+    App::rautiAgurka();
 }
 
 //raunam Pomidora
 if(isset($_POST['rautiP'])) {
-    foreach($_SESSION['objP'] as $index => $pomidoras) {
-        $pomidoras = unserialize($pomidoras);
-        if ($_POST['rautiP'] == $pomidoras->id) {
-            unset($_SESSION['objP'][$index]);
-            App::redirect(sodinimas);
-        }
-    }
+    App::rautiPomidora();
 }
 
 //raunam moliuga
 if(isset($_POST['rautiM'])) {
-    foreach($_SESSION['objM'] as $index => $moliugas) {
-        $moliugas = unserialize($moliugas);
-        if ($_POST['rautiM'] == $moliugas->id) {
-            unset($_SESSION['objM'][$index]);
-            App::redirect(sodinimas);
-        }
-    }
+    App::rautiMoliuga();
 }
 
 ?>

9c92ebc280fe255075c1bad750de83417a9b4db4
BrigitaK <70912981+BrigitaK@users.noreply.github.com>
2021-01-13 15:50:54 +0200

    js

--- js/app.js
+++ js/app.js
@@ -4,9 +4,9 @@ const buttonSodintiM = document.querySelector('[name="sodintiM"]');
 const buttonSodintiP = document.querySelector('[name="sodintiP"]');
 const buttonSodintiV = document.querySelector('[name="sodintiV"]');
 const buttonIsrauti = document.querySelector('.btn-israuti');
-const sodinti = document.querySelector('#sodinti');
+const list = document.querySelector('#list');
 buttonSodinti.addEventListener('click', () =>{
-    const info = document.querySelector('[]');
+    const info = document.querySelector('#cucumber').value;
     
 
     axios.post(apiUrl, {
--- sodinimas.php
+++ sodinimas.php
@@ -10,15 +10,41 @@ $store = new Main\Store('darzoves');
 if('POST' == $_SERVER['REQUEST_METHOD']) {
     $rawData = file_get_contents("php://input");
     $rawData = json_decode($rawData,1);
-}
-// SODINIMO SCENARIJUS AGURKU
 
+    // LISTo SCENARIJUS
+    if (isset($rawData['list'])) {
+        // sleep(3);
+            ob_start();
+            include __DIR__.'/list.php';
+            $out = ob_get_contents();
+            ob_end_clean();
+            $json = ['list' => $out];
+            $json = json_encode($json);
+            header('Content-type: application/json');
+            http_response_code(200);
+            echo $json;
+            die;
+        
+    }
 
+    //sodinimo scenarijus
     if (isset($rawData['sodinti'])) {
         $agurkoObj = new Main\Agurkas($store->getNewId());
         $store->addNew($agurkoObj);
-        Main\App::redirect('sodinimas');
     }
+    //pasodine agurkus jungsim buferi
+    ob_start();
+    include __DIR__.'/list.php';//liepsiu listau sugeneruoti nauja sarasa
+    $out = ob_get_contents();//viskas subegs i buferi
+    ob_end_clean();
+    $json = ['list' => $out];//issiusime agurku lista
+    $json = json_encode($json);
+    header('Content-type: application/json');
+    http_response_code(201);//
+    echo $json;
+    die;
+  
+}
 
 
 if(isset($rawData['sodintiM'])) {
@@ -92,7 +118,7 @@ if(isset($rawData['rautiM'])) {
         <h1>Daržovių sodinimas</h1>
         <div class="container">
             <form class="form" action="<?= URL.'sodinimas' ?>" method="POST">
-                <div  id="sodinti">
+                <div  id="list">
                     <?php foreach($store->getAll() as $agurkas): //paverciam i obj, norint panaudoti reikia isserializuoti?>
                     <div class="form-top">
                         <div class="agurkas-nr">
@@ -125,9 +151,13 @@ if(isset($rawData['rautiM'])) {
             </div>
             <?php endforeach ?>
             <div class="sodinti">
+                <input class="agurkas" type="hidden" name="sodinti" id="cucumber">
                 <button class="btn-sodinti" type="submit" name="sodinti">SODINTI AGURKUS</button>
+                <input class="pomidoras" type="hidden" name="sodintiP" id="tomato">
                 <button class="btn-sodinti" type="submit" name="sodintiP">SODINTI POMIDORUS</button>
+                <input class="moliugas" type="hidden" name="sodintiM" id="pumpkin">
                 <button class="btn-sodinti" type="submit" name="sodintiM">SODINTI MOLIŪGUS</button>
+                <input class="darzoves" type="hidden" name="sodintiV" id="allV">
                 <button class="btn-sodinti" type="submit" name="sodintiV">SODINTI VISUS</button>
             </div>
             </form>

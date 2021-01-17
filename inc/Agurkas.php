562e337a65b71fe9e8f9999f59619f37b0b34bd3
BrigitaK <70912981+BrigitaK@users.noreply.github.com>
2021-01-17 20:57:43 +0200

    +

--- inc/Agurkas.php
+++ inc/Agurkas.php
@@ -11,7 +11,7 @@ class Agurkas extends Darzove {
         $photos = array("./photo/agurkas1.jpg", "./photo/agurkas3.jpg", "./photo/agurkas2.jpg");
         $this->photo = $photos[array_rand($photos)];
         $this->priceE = 1.2;
-        $this->dKursas = 0;
+        $this->dKursas = 1.21;
     }
     
 
@@ -21,18 +21,11 @@ class Agurkas extends Darzove {
 
     public function priceD()
     {
-        $ch = curl_init();
-
-        curl_setopt(
-        $ch, CURLOPT_URL, 'https://free.currconv.com/api/v7/convert?q=EUR_USD&compact=ultra&apiKey=fcb8cfbc519ccc63fe6f'
-        );//pasako kur mes eisime
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);//returninam
-
-        $answer = curl_exec($ch); // siuntimas ir laukimas atsakymo
-
-        $answer = json_decode($answer);
-
-        $this->dKursas = $answer;
-        return $this->priceE * $this->dKursas;
+        $url = file_get_contents("https://free.currconv.com/api/v7/convert?q=eur_usd&compact=ultra&apiKey=fcb8cfbc519ccc63fe6f");
+        $json = json_decode($url, true);
+        $rate = implode(" ",$json);
+        $total = $rate * $amount;
+        $rounded = round($total,2);
+        return $this->priceE * $rounded;
     }
 }

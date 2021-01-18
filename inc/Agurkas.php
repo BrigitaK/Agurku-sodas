f2b50e3c1ecc5a9e248d7f44ad39141dcc4c4a00
BrigitaK <70912981+BrigitaK@users.noreply.github.com>
2021-01-18 19:13:46 +0200

    price

--- inc/Agurkas.php
+++ inc/Agurkas.php
@@ -11,7 +11,6 @@ class Agurkas extends Darzove {
         $photos = array("./photo/agurkas1.jpg", "./photo/agurkas3.jpg", "./photo/agurkas2.jpg");
         $this->photo = $photos[array_rand($photos)];
         $this->priceE = 1.2;
-        $this->dKursas = 1.21;
     }
     
 
@@ -21,12 +20,11 @@ class Agurkas extends Darzove {
 
     public function priceD()
     {
-        // $url = file_get_contents("https://free.currconv.com/api/v7/convert?q=eur_usd&compact=ultra&apiKey=fcb8cfbc519ccc63fe6f");
-        // $json = json_decode($url, true);
-        // $rate = implode(" ",$json);
-        // $total = $rate * $amount;
-        // $rounded = round($total,2);
-        $total = $this->priceE * $this->dKursas;
+        $this->priceE = 1.2;
+        $url = file_get_contents("https://free.currconv.com/api/v7/convert?q=eur_usd&compact=ultra&apiKey=fcb8cfbc519ccc63fe6f");
+        $json = json_decode($url, true);
+        $rate = implode(" ",$json);
+        $total = $rate * $this->priceE;
         $rounded = round($total,2);
         return $rounded;
     }
--- inc/Moliugas.php
+++ inc/Moliugas.php
@@ -11,7 +11,6 @@ class Moliugas extends Darzove {
         $photos = array("./photo/moliugas.jpg", "./photo/moliugas1.jpg", "./photo/moliugas2.jpg");
         $this->photo = $photos[array_rand($photos)];
         $this->priceE = 1.8;
-        $this->dKursas = 1.21;
     }
 
     public function auga() {
@@ -19,12 +18,10 @@ class Moliugas extends Darzove {
     }
     public function priceD()
     {
-        // $url = file_get_contents("https://free.currconv.com/api/v7/convert?q=eur_usd&compact=ultra&apiKey=fcb8cfbc519ccc63fe6f");
-        // $json = json_decode($url, true);
-        // $rate = implode(" ",$json);
-        // $total = $rate * $amount;
-        // $rounded = round($total,2);
-        $total = $this->priceE * $this->dKursas;
+        $url = file_get_contents("https://free.currconv.com/api/v7/convert?q=eur_usd&compact=ultra&apiKey=fcb8cfbc519ccc63fe6f");
+        $json = json_decode($url, true);
+        $rate = implode(" ",$json);
+        $total = $rate * $this->priceE;
         $rounded = round($total,2);
         return $rounded;
     }

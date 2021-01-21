2416e9ab78dec109d36d51f6b5323e3396e14204
BrigitaK <70912981+BrigitaK@users.noreply.github.com>
2021-01-21 10:50:42 +0200

    ex

---
+++ ex/index.php
@@ -0,0 +1,24 @@
+<?php
+
+$z = 0;
+// class A{}
+// $a = new A;
+// $a->z();
+
+class customException extends Exception {}
+
+try {
+    if($z===0) {
+        throw new customException("Value must be not 0");
+    }
+
+echo 1/$z;
+}
+catch (Exception $e) {
+    echo '<br>Message: ' .$e->getMessage();
+}
+// catch (DivisionByZeroError $e) {
+//     echo "Caught DivisionByZeroError!\n";
+// }
+
+echo '<br>Gero savaitgalio';

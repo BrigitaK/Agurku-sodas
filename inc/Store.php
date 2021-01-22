1dfb3e05ef70fb5ef2bc7690174bdce49e10925f
BrigitaK <70912981+BrigitaK@users.noreply.github.com>
2021-01-22 23:26:40 +0200

    db

--- inc/Store.php
+++ inc/Store.php
@@ -8,9 +8,15 @@ interface Store
     // function getData();
     // function setData($data);
     function getNewId();
-    function addNew(Agurkas $obj);
+    function addNew(Agurkas $objA);
+    function addNewM(Moliugas $objM);
+    function addNewP(Moliugas $objP);
     function getAll();
+    function getAllP();
+    function getAllM(); 
     function remove($id);
+    function removeM($id);
+    function removeP($id);
 
 
 }
